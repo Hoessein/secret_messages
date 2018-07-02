efe57f9adac4d4378061619131731cb7be5899b9
Hoessein Abdullahi <habdullahi@saa.nl>
2018-07-03 00:04:47 +0200

    added the logic for the keyword cipher.

--- keywrd.py
+++ keywrd.py
@@ -1,14 +1,47 @@
 from cipher import Cipher
 
+
 class Keyword(Cipher):
+    # list comprehensions saved in variables to use later.
 
-    #list comprehensions saved in variables to use later.
-    alphabet_list = [letter for letter in 'abcdefghijklmnopqrstuvwxyz']
-    encrypted_list = [keyletter for keyletter in 'kryptosabcdefghijlmnquvwxz']
+    def __init__(self):
+        self.encrypted_list = self.keyword_pad()
+        self.alphabet_list = [letter for letter in 'abcdefghijklmnopqrstuvwxyz']
 
-    #dict comprehensions. I can enter the value of the key in one of the dict and the key's value in the other dict.
-    keyword_encrypt_dict = {letter: keyletter for letter, keyletter in zip(alphabet_list, encrypted_list)}
-    keyword_decrypt_dict = {keyletter: letter for keyletter, letter in zip(encrypted_list, alphabet_list)}
+    def encrypted_dict(self):
+        """dict comprehension so i can enter the dict's value's and key's easily."""
+        keyword_encrypt_dict = {letter: keyletter for letter, keyletter in zip(self.alphabet_list, self.encrypted_list)}
+        return keyword_encrypt_dict
+
+    def decrypted_dict(self):
+        """a simple dict comprehension, love these"""
+        keyword_decrypt_dict = {keyletter: letter for keyletter, letter in zip(self.encrypted_list, self.alphabet_list)}
+        return keyword_decrypt_dict
+
+    def keyword_pad(self):
+        """first i make an empty list and ask the user to enter a keyword
+            then i loop through the prompt and place them in a empty variable "holder"
+            then i loop through "holder" and check if any value in "holder" is in the alphabet_list
+            those items then get removed.
+            In the loop I also append each item in "holder" to encrypted_list_copy
+            and finally extend encrypted_list_copy to alphabet_list"""
+
+        encrypted_list_copy = []
+
+        keyword = input("\nPlease enter a keyword: ")
+        holder = ""
+
+        for a in keyword:
+            holder += a
+
+        for x in holder:
+            if x in self.alphabet_list:
+                self.alphabet_list.remove(x)
+                encrypted_list_copy.append(x)
+
+        encrypted_list_copy.extend(self.alphabet_list)
+
+        return encrypted_list_copy
 
     def encrypt(self):
         """ prompts user to enter a message
@@ -20,8 +53,8 @@ class Keyword(Cipher):
         output = ""
         message = input("\nEnter your message: ").lower()
         for letter in message:
-            if letter in self.keyword_encrypt_dict:
-                output += str(self.keyword_encrypt_dict[letter])
+            if letter in self.encrypted_dict():
+                output += str(self.encrypted_dict()[letter])
             else:
                 output += letter
         print("\nHere is your encryption:", output.upper())
@@ -36,8 +69,11 @@ class Keyword(Cipher):
         output = ""
         message = input("\nEnter your message: ").lower()
         for letter in message:
-            if letter in self.keyword_decrypt_dict:
-                output += str(self.keyword_decrypt_dict[letter])
+            if letter in self.decrypted_dict():
+                output += str(self.decrypted_dict()[letter])
             else:
                 output += letter
         print("\nHere is your decryption:", output.upper())
+
+
+
